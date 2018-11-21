49fa9a4362e6331bb22445a93e33ee6f286a909e
Andres <contact@andresbott.com>
2018-11-21 22:34:21 +0100

    add mysql install

--- setup.bash
+++ setup.bash
@@ -61,6 +61,17 @@ sudo rm -f /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
 #=============================================================================================================
+# configure Mysql
+#=============================================================================================================
+sudo apt-get update
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
+sudo apt-get -y install mysql-server
+mysql -u root -proot -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION; FLUSH PRIVILEGES; SET GLOBAL max_connect_errors=10000;"
+sudo /etc/init.d/mysql restart
+
+
+#=============================================================================================================
 # Ending Provisioning
 #=============================================================================================================
 IP=`ip addr show eth0 | grep -Po 'inet \K[\d.]+'`
