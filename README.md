a29af3f6a10e9bc889409b182d6c2b24291a6ad7
Andres <contact@andresbott.com>
2018-11-21 22:28:03 +0100

    initial commit with basic project

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # vagrant-php-dev
 
 Vagrant project for creating reusable php dev environment
+
+# config
+vagrant file will check for ../vagrantConfig.yaml before falling back to ./vagrantConfig.yaml 
+
+This is in order to clone the vagrant project in a sub folder to the project folder
+
+same applies to nginx-site.conf an php-poo.conf; the setup will search for the files in the mounted src dir before 
+falling back to ./   
---
+++ setup.bash
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+#=============================================================================================================
+## add a better bashrc for root shell
+#=============================================================================================================
+sudo cp /vagrant/rootbashrc /root/.bashrc
+sudo chown root:root /root/.bashrc
+
+#=============================================================================================================
+## install apps
+#=============================================================================================================
+sudo apt-get update
+sudo apt-get install -y nginx-full php-fpm joe htop curl
+
+## Clean files
+sudo rm -f /etc/php/7.0/fpm/pool.d/www.conf
+sudo rm -f /etc/php/7.0/fpm/pool.d/phpuser1-pool.conf
+
+
+## create vhost dir and user
+sudo mkdir -p /vhosts
+
+#=============================================================================================================
+# Create user
+#=============================================================================================================
+USER="web"
+sudo id -u $USER &>/dev/null || useradd -d /vhosts -u 1100 $USER
+sudo mkdir -p /vhosts/$USER/public_html
+sudo chown -R $USER:$USER /vhosts/$USER/
+sudo chmod -R 750 /vhosts/$USER/
+
+sudo usermod -a -G $USER www-data
+
+#BINDMOUNT="/host_project/php/ /vhosts/$USER/public_html/ none defaults,bind 0 0"
+#sudo grep -q -F "$BINDMOUNT" /etc/fstab || echo "$BINDMOUNT" >> /etc/fstab
+#sudo mount -a
+
+
+
+#=============================================================================================================
+# configure php
+#=============================================================================================================
+if [ -f /host_project/php-pool.conf ]; then
+  sudo cp /host_project/php-pool.conf /etc/php/7.0/fpm/pool.d/
+else
+  sudo cp /vagrant/php-pool.conf /etc/php/7.0/fpm/pool.d/
+fi
+sudo /etc/init.d/php7.0-fpm restart
+
+
+#=============================================================================================================
+# configure nginx
+#=============================================================================================================
+if [ -f /host_project/nginx-site.conf ]; then
+  sudo cp /host_project/nginx-site.conf /etc/nginx/sites-available/
+else
+  sudo cp /vagrant/nginx-site.conf  /etc/nginx/sites-available/
+fi
+
+sudo ln -sf /etc/nginx/sites-available/nginx-site.conf /etc/nginx/sites-enabled/nginx-site.conf
+sudo rm -f /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx restart
+
+#=============================================================================================================
+# Ending Provisioning
+#=============================================================================================================
+IP=`ip addr show eth0 | grep -Po 'inet \K[\d.]+'`
+
+echo ""
+echo ""
+echo ""
+echo "PHP FPM machine configured with IP: $IP"
